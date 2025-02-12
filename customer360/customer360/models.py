b4acee74047390d7239f3a75cecc986aa345c5ff
kimalysha93 <kimalysha93@gmail.com>
2025-02-12 12:49:10 -0500

    added social media field

--- customer360/customer360/models.py
+++ customer360/customer360/models.py
@@ -6,6 +6,7 @@ class Customer(models.Model):
     email = models.EmailField(max_length=100)
     phone = models.CharField(max_length=20)
     address = models.CharField(max_length=200)
+    social_media = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
         return str(self.id)
@@ -17,6 +18,7 @@ class Interaction(models.Model):
         ('sms', 'SMS'),
         ('email', 'Email'),
         ('letter', 'Letter'),
+        ('social_media', 'Social Media'),
     ]
 
     DIRECTION_CHOICES = [
